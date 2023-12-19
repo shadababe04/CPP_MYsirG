a09b723ff25109c4afe50fe23335d784a2a75360
MOHAMMAD SHADAB ABEDIN <shadababe04@gmail.com>
2023-12-19 18:03:24 +0530

    calculate string length

--- class_String/string.c
+++ class_String/string.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 int main()
 {
+    int i;
+    /*
     char str[10]={'B','H','O','P','A','L'};
-    for(int i=0;str[i];i++)
+    for(i=0;str[i];i++)
     printf("%c",str[i]);
+    */
+    //!Calculate length of string
+    char str[10]="BHOPAL";
+    for(i=0; str[i];i++);
+        printf("%s\n",str);
+    printf("String length= %d",i);
     return 0;
 }
