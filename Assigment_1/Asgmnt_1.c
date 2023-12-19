c3e95e5ae9648fb9b4b2ab5a8f0008c9c8065e56
MOHAMMAD SHADAB ABEDIN <shadababe04@gmail.com>
2023-12-19 16:34:51 +0530

    Complete Assigment 1 of C lang

---
+++ Assigment_1/Asgmnt_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    /*
+    //!Question 1
+    //printf("Hello");
+
+    //!Question 2
+    printf("Hello \nStudent");
+    
+    //!Question 3
+    printf("\" MySirG \"");
+    
+    //!Question 4
+    printf("\\n");
+    */
+    //!Question 5
+    printf("\\r");
+    return 0;
+}
